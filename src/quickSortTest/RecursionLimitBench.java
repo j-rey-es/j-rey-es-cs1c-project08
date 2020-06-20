db28bb66f76438d7b176b9e3e5c7178e64df64a2
Joel Reyes <its.j.reyes@gmail.com>
2020-06-19 17:58:31 -0700

    Created generateRandomArray, qSortRuntime

--- src/quickSortTest/RecursionLimitBench.java
+++ src/quickSortTest/RecursionLimitBench.java
@@ -1,81 +1,66 @@
 package quickSortTest;
 
 
+import java.time.chrono.MinguoChronology;
+
 public class RecursionLimitBench
 {
-    public static double compareSortAlgos(int testSize)
-    {
-        int k, randomInt;
-        long startTime, estimatedTime1, estimatedTime2;
+    public static Integer[] generateRandomArray(int size){
+        int i;
+        Integer RandomArray[] = new Integer [size];
+        for (i = 0; i<RandomArray.length; i++)
+            RandomArray[i] = (int) (Math.random() * size);
+        return RandomArray;
 
-        Integer[] arrayOfInts1 = new Integer[testSize];
-        Integer[] arrayOfInts2 = new Integer[testSize];
-
-        // build two arrays for comparing sorts
-        for (k = 0; k < testSize; k++)
-        {
-            randomInt = (int) (Math.random() * testSize);
-            arrayOfInts1[k] = randomInt;
-            arrayOfInts2[k] = randomInt;
-        }
-        startTime = System.nanoTime();  // ------------------ start 
-
-        FHsort.heapSort(arrayOfInts1);
-
-        estimatedTime1 = System.nanoTime() - startTime;    // ---------------------- stop
-        System.out.println("Heap sort Elapsed Time: "
-                + " size: " + testSize + ", "
-                + estimatedTime1
-                + " = " + estimatedTime1 + "ns");
-
-        startTime = System.nanoTime();  // ------------------ start 
-
-        FHsort.quickSort(arrayOfInts2);
-
-
-        estimatedTime2 = System.nanoTime() - startTime;    // ---------------------- stop
-        System.out.println("Quick sort Elapsed Time: "
-                + " size: " + testSize + ", "
-                + estimatedTime2
-                + " = " + estimatedTime2 + "ns");
-
-        // Note: un-comment  to verify sort
-        //displayArray(arrayOfInts1, "Heap");
-        //displayArray(arrayOfInts2, "Quick");
-
-        return (double)estimatedTime1 / (double)estimatedTime2;
     }
 
-
-    public static void displayArray(Integer [] theArray, String message)
+    public static double qSortRuntime (Integer[] testArray)
     {
-        for (int k = 0; k < theArray.length; k+= theArray.length/10)
+        long startTime, estimatedTime = 0;
+        int i;
+        final double NUM_OF_TRIALS = 3;
+        for (i = 0; i < NUM_OF_TRIALS ; i++)
         {
-            System.out.println( message + " #" + k + ": " + theArray[k] + "");
+            startTime = System.nanoTime();  // ------------------ start
+            FHsort.quickSort(testArray);
+            estimatedTime += System.nanoTime() - startTime;
         }
-    }
+        return estimatedTime/NUM_OF_TRIALS;
 
+    }
 
     // -------  main --------------
     public static void main(String[] args) throws Exception
     {
 
-        int [] ARRAY_SIZES;
-        ARRAY_SIZES = new int[20];
-        int size = 500000;
+        int [] arraySizes = {20000,50000,100000,200000,500000,1000000,5000000,10000000};
+        final int MIN_ARRAY_SIZE = 200000;
+        final int MAX_ARRAY_SIZE = 10000000;
+        final int NUM_OF_SIZE_INTERVALS = 20;
+        final int MIN_RECURSION_LIMIT =  2;
+        final int MAX_RECURSION_LIMIT = 300;
+        //arraySizes = new int[NUM_OF_SIZE_INTERVALS];
 
-        for (int i = 0; i<ARRAY_SIZES.length;  i++)
-        {
-            ARRAY_SIZES[i] = size;
-            size = size + 500000;
-        }
-        double ratio;
+        Integer randomArray [] = generateRandomArray(MAX_ARRAY_SIZE);
 
-        for (int test = 0; test < ARRAY_SIZES.length; test++)
+       /* for (int i = 0; i<arraySizes.length;  i++)
         {
-            int currentSize = ARRAY_SIZES[test];
-            ratio = compareSortAlgos(currentSize);
-            System.out.printf("For data size %d ratio = %.2f \n\n", currentSize, ratio);
+            arraySizes[i] = MIN_ARRAY_SIZE + (((MAX_ARRAY_SIZE - MIN_ARRAY_SIZE) / (NUM_OF_SIZE_INTERVALS)) * i);
+        } */
+
+        for (int j = MIN_RECURSION_LIMIT; j < MAX_RECURSION_LIMIT; j = j+2)
+        {
+            String runtimeRow = new String();
+            FHsort.setRecursionLimit(j);
+            System.out.print(j + ",");
+                for (int k = 0; k < arraySizes.length; k++)
+                {
+                    Integer [] arrayOfInts = new Integer[arraySizes[k]];
+                    for (int n = 0; n <  arrayOfInts.length; n++)
+                        arrayOfInts[n] = randomArray[n];
+                    runtimeRow = runtimeRow + qSortRuntime(arrayOfInts) + ",";
+                }
+            System.out.println(runtimeRow);
         }
     }
 }
